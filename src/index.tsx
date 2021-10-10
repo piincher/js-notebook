f0fb8ca68bd9dbba160bfc861e19d638306f614e
Ibrahim Kouma <69568495+piincher@users.noreply.github.com>
2021-10-10 17:14:08 +0800

    transpiling works

--- src/index.tsx
+++ src/index.tsx
@@ -17,11 +17,15 @@ const App = () => {
 	useEffect(() => {
 		startService();
 	}, []);
-	const onClick = () => {
+	const onClick = async () => {
 		if (!ref.current) {
 			return;
 		}
-		console.log(ref.current);
+		const result = await ref.current.transform(input, {
+			loader: 'jsx',
+			target: 'es2015'
+		});
+		setCode(result.code);
 	};
 	return (
 		<div>
