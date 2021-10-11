07d0c1a8f54d54508b7c59365d454714cb4b0a97
Ibrahim Kouma <69568495+piincher@users.noreply.github.com>
2021-10-11 22:04:48 +0800

    esbuild can bundle

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as esbuild from 'esbuild-wasm';
 import { useState, useEffect, useRef } from 'react';
 import ReactDom from 'react-dom';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
 const App = () => {
 	const ref = useRef<any>();
@@ -21,11 +22,14 @@ const App = () => {
 		if (!ref.current) {
 			return;
 		}
-		const result = await ref.current.transform(input, {
-			loader: 'jsx',
-			target: 'es2015'
+		const result = await ref.current.build({
+			entryPoints: [ 'index.js' ],
+			bundle: true,
+			write: false,
+			plugins: [ unpkgPathPlugin() ]
 		});
-		setCode(result.code);
+		//console.log(result);
+		setCode(result.outputFiles[0].text);
 	};
 	return (
 		<div>
