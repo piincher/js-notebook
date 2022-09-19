b6b3db5b33fab12b8b475eda9536ecfae75e1417
Ibrahim <ikouma269@gmail.com>
2022-09-19 15:24:42 +0800

    dynamic fetching modules

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,12 +1,22 @@
 import * as esbuild from "esbuild-wasm";
-
+import axios from "axios";
 export const unpkgPathPlugin = () => {
   return {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
         console.log("onResole", args);
-        return { path: args.path, namespace: "a" };
+
+        if (args.path === "index.js") {
+          return { path: args.path, namespace: "a" };
+        } else if (args.path === "tiny-test-pkg") {
+          return {
+            path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js",
+            namespace: "a",
+          };
+        } else {
+          return { path: args.path, namespace: "a" };
+        }
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -16,16 +26,17 @@ export const unpkgPathPlugin = () => {
           return {
             loader: "jsx",
             contents: `
-              import message from './message';
+              const  message = require('tiny-test-pkg')
               console.log(message);
             `,
           };
-        } else {
-          return {
-            loader: "jsx",
-            contents: 'export default "hi there!"',
-          };
         }
+
+        const { data } = await axios.get(args.path);
+        return {
+          loader: "jsx",
+          contents: data,
+        };
       });
     },
   };
